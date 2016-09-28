245d982b71d3631eefeb4466d4763e3032cc9e0a
Rae Helsel <rahelsel@me.com>
2016-09-28 16:54:53 -0700

    added env.sh and reworked request with ajax

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,30 +4,20 @@
   repos.requestRepos = function(callback) {
     // TODO: refactor these requests into an $.ajax call
     $.when(
-     $.ajax({
-       url: 'https://api.github.com/users/codefellows-seattle-301d12/repos' +
-            '?per_page=10' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
+     $.get('/github/users/codefellows-seattle-301d12')
+       .done(function(data) {
+         repos.allRepos = data;
+       }),
+  $.get('/github/users/patci/followers' +
+         '?per_page=5' +
+         '&sort=updated')
+         .done(function(data) {
+           repos.followers = data;
+         })
+ ).done(callback);
+  };
          // NOTE: since the 'data' paramter comes back as an
          // array of objects, we can reassign allRepos below.
-         repos.allRepos = data;
-       }
-     }),
-     $.ajax({
-       url: 'https://api.github.com/users/patci/followers' +
-            '?per_page=5' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         repos.followers = data;
-       }
-     })
-    ).done(callback);
-  };
 
   repos.withTheAttribute = function(attr) {
     return repos.allRepos.filter(function(aRepo) {
